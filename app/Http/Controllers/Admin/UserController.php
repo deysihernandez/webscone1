70341675721fb6ebdfd6795022234b9b762a10ad
deysihernandez <deysiihernandezz15@gmail.com>
2020-05-26 10:56:40 +0200

    agregando UserController.php....

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function __construct(){
+        $this->middleware('auth');
+        $this->middleware('isadmin');
+   }
+
+   public function getUsers(){
+       return view('admin.users.user');
+   }
+}
